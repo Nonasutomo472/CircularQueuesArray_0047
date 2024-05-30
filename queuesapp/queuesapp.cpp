0e63ae472bdfee22fd235e21bd9c52405a97ae38
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-05-30 10:43:05 +0700

    Add project files.

---
+++ queuesapp/queuesapp.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+using namespace std;
+
+// CircularQueuesArray_0047
+// Minimal 15 commit
+class Queues {
+	int FRONT, REAR, max = 5;
+	int queue_array[5];
+
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "enter a number :";
+		cin >> num;
+		cout << endl;
+
+		//cek apakah antrian penuh 
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overFlow\n";
+			return;
+		}
+
+		//cek apakah antrian kosong 
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+
+	void remove() {
+
+	}
+
+};
